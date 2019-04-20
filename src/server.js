478229946319678d436e4e3b4ea69005d4485ab5
astrosalinas <astrosalinas35@gmail.com>
2019-04-20 14:17:12 -0400

    config docker, and database

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dbConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
+  }
+
+  database () {
+    mongoose.connect(dbConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
   middlewares () {
