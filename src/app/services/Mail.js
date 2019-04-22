3e2126fdeabd679235c1345cd34cfe66351e8009
astrosalinas <astrosalinas35@gmail.com>
2019-04-22 15:33:40 -0400

    add redis and email send, template not fount

--- src/app/services/Mail.js
+++ src/app/services/Mail.js
@@ -1,6 +1,20 @@
+// const fs = require('fs')
+// const hbs = require('handlebars')
 const nodemailer = require('nodemailer')
+const path = require('path')
+const hbs = require('nodemailer-express-handlebars')
+const exphbs = require('express-handlebars')
 const mailConfig = require('../../config/mail')
 
 const transport = nodemailer.createTransport(mailConfig)
+
+transport.use(
+  'compile',
+  hbs({
+    viewEngine: exphbs(),
+    viewPath: path.resolve(__dirname, '..', 'views', 'emails'),
+    extName: '.hbs'
+  })
+)
 
 module.exports = transport
